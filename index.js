e5c0ca10fabf9648809cc21a54a7be4ce73518b3
Pour Le Hommes <Pour-le-hommes@terra.com>
2023-04-05 13:15:49 +0700

    tes 6

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const { Client } = require('@line/bot-sdk');
 const express = require('express')
-const app = express()
+const bodyParser = require('body-parser');
+
+const app = express();
+app.use(bodyParser.json());
 
 const CHANNEL_ACCESS_TOKEN = 'qkj/lPvrRK5+BEleRc7d3MUv+P8GNNWxsJOE1+mHYXxCtVreNjPrkUs84z1M/6YC3iT99ORZ+8oFQur+e65c9KU8cOJF7p5sXYluNl27t4Vf7BtmXOVzwHeirh7riGCg1xhESoSqpB0Pbq3d5MNnKAdB04t89/1O/w1cDnyilFU=';
 const CHANNEL_SECRET = 'fe50c21e3a689c8ce8227c63545f3f51';
@@ -36,4 +39,7 @@ app.post('/webhook', (req, res) => {
   }
 
   Promise.all(promises).then(() => res.status(200).end());
+});
+app.listen(3000, () => {
+  console.log('Server listening on port 3000');
 });
