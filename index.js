7128c0a1fcfa327fde9c6407f1d5ce4faf86b325
Pour Le Hommes <Pour-le-hommes@terra.com>
2023-04-05 13:08:31 +0700

    tes 4

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const { Client } = require('@line/bot-sdk');
+const app = express()
 
 const CHANNEL_ACCESS_TOKEN = 'qkj/lPvrRK5+BEleRc7d3MUv+P8GNNWxsJOE1+mHYXxCtVreNjPrkUs84z1M/6YC3iT99ORZ+8oFQur+e65c9KU8cOJF7p5sXYluNl27t4Vf7BtmXOVzwHeirh7riGCg1xhESoSqpB0Pbq3d5MNnKAdB04t89/1O/w1cDnyilFU=';
 const CHANNEL_SECRET = 'fe50c21e3a689c8ce8227c63545f3f51';
