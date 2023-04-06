0f10bb2a54cbbef83ddbb7d0420978b8ebe2c302
Pour Le Hommes <Pour-le-hommes@terra.com>
2023-04-06 11:15:50 +0700

    add client line

--- webhook/webhook.js
+++ webhook/webhook.js
@@ -1,4 +1,15 @@
 function webhook(req,res) {
+    const { Client } = require('@line/bot-sdk');
+    const CHANNEL_ACCESS_TOKEN = 'qkj/lPvrRK5+BEleRc7d3MUv+P8GNNWxsJOE1+mHYXxCtVreNjPrkUs84z1M/6YC3iT99ORZ+8oFQur+e65c9KU8cOJF7p5sXYluNl27t4Vf7BtmXOVzwHeirh7riGCg1xhESoSqpB0Pbq3d5MNnKAdB04t89/1O/w1cDnyilFU=';
+    const CHANNEL_SECRET = 'fe50c21e3a689c8ce8227c63545f3f51';
+
+    const client = new Client({
+    channelAccessToken: CHANNEL_ACCESS_TOKEN,
+    channelSecret: CHANNEL_SECRET,
+    });
+
+
+
     const Massaterra = require('./massa.js');
     const oaterra = require("./oa.js");
     const pengurusterra = require("./pengurus.js");
