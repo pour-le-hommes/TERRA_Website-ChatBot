10efccf67bd3bb27de9fce397e0fc9ce408205df
Pour Le Hommes <Pour-le-hommes@terra.com>
2023-04-08 01:02:45 +0700

    testing webhook

--- server.js
+++ server.js
@@ -2,19 +2,19 @@ const express = require('express')
 const app = express()
 // const mongoose = require('mongoose')
 const UserRouter = require('./routes/users')
-// const { Client } = require('@line/bot-sdk')
-// const webhook = require('./webhook/webhook.js')
+const { Client } = require('@line/bot-sdk')
+const webhook = require('./webhook/webhook.js')
 // const bodyParser = require('body-parser');
 
 // app.use(bodyParser.json());
 
-// const CHANNEL_ACCESS_TOKEN = process.env.CHANNEL_ACCESS_TOKEN;
-// const CHANNEL_SECRET = process.env.CHANNEL_SECRET;
+const CHANNEL_ACCESS_TOKEN = process.env.CHANNEL_ACCESS_TOKEN;
+const CHANNEL_SECRET = process.env.CHANNEL_SECRET;
 
-// const client = new Client({
-//   channelAccessToken: CHANNEL_ACCESS_TOKEN,
-//   channelSecret: CHANNEL_SECRET,
-// });
+const client = new Client({
+  channelAccessToken: CHANNEL_ACCESS_TOKEN,
+  channelSecret: CHANNEL_SECRET,
+});
 
 // Connect To Mongodb
 // const url = 'mongodb+srv://testing:testing123@cluster0.ytucosn.mongodb.net/?retryWrites=true&w=majority'
@@ -24,8 +24,8 @@ const UserRouter = require('./routes/users')
 //     .catch((err) => console.log(err))
 
 // // Create View Engine
-// console.log('View Engine')
-// app.set('view engine', 'ejs')
+console.log('View Engine')
+app.set('view engine', 'ejs')
 
 // Create Home Page
 app.get('/',(req,res) =>{
@@ -39,15 +39,15 @@ app.listen(process.env.PORT||3000)
 // Users Pages
 // app.use('/users', UserRouter)
 
-// app.post('/webhook', (req, res) => {
-//   console.log(req.session)  
-//   console.log(req.session.EmotionalState)
-//   const promises = [];
-//   const events = req.body.events;
-//   for (let i = 0; i < events.length; i++) {
-//     const event = events[i];
-//     message = webhook(event,req)
-//     promises.push(client.replyMessage(event.replyToken, message));
-//   }
-//   Promise.all(promises).then(() => res.status(200).end());
-// });
+app.post('/webhook', (req, res) => {
+  console.log(req.session)  
+  console.log(req.session.EmotionalState)
+  const promises = [];
+  const events = req.body.events;
+  for (let i = 0; i < events.length; i++) {
+    const event = events[i];
+    message = webhook(event,req)
+    promises.push(client.replyMessage(event.replyToken, message));
+  }
+  Promise.all(promises).then(() => res.status(200).end());
+});
