2faa208b311ef86c226ca36ddb69f06a63aa1636
Pour Le Hommes <Pour-le-hommes@terra.com>
2023-04-07 20:41:05 +0700

    add line

--- server.js
+++ server.js
@@ -2,19 +2,19 @@ const express = require('express')
 const app = express()
 const mongoose = require('mongoose')
 // const UserRouter = require('./routes/users')
-// const { Client } = require('@line/bot-sdk')
+const { Client } = require('@line/bot-sdk')
 // const webhook = require('./webhook/webhook.js')
 // const bodyParser = require('body-parser');
 
 // app.use(bodyParser.json());
 
-// const CHANNEL_ACCESS_TOKEN = 'qkj/lPvrRK5+BEleRc7d3MUv+P8GNNWxsJOE1+mHYXxCtVreNjPrkUs84z1M/6YC3iT99ORZ+8oFQur+e65c9KU8cOJF7p5sXYluNl27t4Vf7BtmXOVzwHeirh7riGCg1xhESoSqpB0Pbq3d5MNnKAdB04t89/1O/w1cDnyilFU=';
-// const CHANNEL_SECRET = 'fe50c21e3a689c8ce8227c63545f3f51';
+const CHANNEL_ACCESS_TOKEN = process.env.CHANNEL_ACCESS_TOKEN;
+const CHANNEL_SECRET = process.env.CHANNEL_SECRET;
 
-// const client = new Client({
-//   channelAccessToken: CHANNEL_ACCESS_TOKEN,
-//   channelSecret: CHANNEL_SECRET,
-// });
+const client = new Client({
+  channelAccessToken: CHANNEL_ACCESS_TOKEN,
+  channelSecret: CHANNEL_SECRET,
+});
 
 // Connect To Mongodb
 const url = 'mongodb+srv://testing:testing123@cluster0.ytucosn.mongodb.net/?retryWrites=true&w=majority'
