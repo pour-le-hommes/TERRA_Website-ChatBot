f9749d36b83b650d770f94d911a3a5b0f5c673d9
Pour Le Hommes <Pour-le-hommes@terra.com>
2023-04-07 19:52:31 +0700

    is parser the problem?

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const { Client } = require('@line/bot-sdk')
 const webhook = require('./webhook/webhook.js')
 const bodyParser = require('body-parser');
 
-app.use(bodyParser.json());
+// app.use(bodyParser.json());
 
 const CHANNEL_ACCESS_TOKEN = 'qkj/lPvrRK5+BEleRc7d3MUv+P8GNNWxsJOE1+mHYXxCtVreNjPrkUs84z1M/6YC3iT99ORZ+8oFQur+e65c9KU8cOJF7p5sXYluNl27t4Vf7BtmXOVzwHeirh7riGCg1xhESoSqpB0Pbq3d5MNnKAdB04t89/1O/w1cDnyilFU=';
 const CHANNEL_SECRET = 'fe50c21e3a689c8ce8227c63545f3f51';
@@ -29,10 +29,8 @@ app.set('view engine', 'ejs')
 
 // Create Home Page
 app.get('/',(req,res) =>{
-    res.send('What"s up?')
     console.log('Home Page')
-    // res.render('index',{text:'suppp'})
-    res.status(200)
+    res.render('index',{text:'suppp'})
 })
 
 // Users Pages
