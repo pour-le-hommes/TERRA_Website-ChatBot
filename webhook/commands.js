3adb1683b832b11dd0f069fcd17dd59ee9e26545
Pour Le Hommes <Pour-le-hommes@terra.com>
2023-04-11 16:39:52 +0700

    testing command

--- webhook/commands.js
+++ webhook/commands.js
@@ -2,14 +2,13 @@ function commands(text,lineid){
     const lineschema = require('../model/line.js');
     const Line = require('../model/line.js');
  
-    // EDIT NAMA JADI 1 DULU
     // EDIT NAMA INCLUDE TERRA GA BISA DIGANTI
     // EDIT REPLACE DATABASE JD FUNCTION
     // EDIT BANYAKLAH ANJING
     console.log('command activated')
     
     if(text.includes('rename')){
-        Line.find({lineid:lineid}).then((result) =>{
+        Line.find({lineid:lineid}).then(() =>{
             const splittext = text.split(" ")
             let message={
                 type:'text',
@@ -20,28 +19,17 @@ function commands(text,lineid){
                     type:'text',
                     text:`Use !rename [name], not just ${text} dumbass!`
                 }
+                return message
             }else{
-                let newname=''
-                if(splittext.length===4){
-                    newname = splittext[1]+' '+splittext[2]+' '+splittext[3]
-                }if(splittext.length===3){
-                    newname = splittext[1]+' '+splittext[2]
-                }else if(splittext.length===2){
-                    newname = splittext[1]
-                }else{
-                    console.log('Text is')
-                    message={
-                        type:'text',
-                        text:`woy ${text}, seriuslah, panjang kali namamu anjing, ini bukan paragraf`
-                    }
-                }
+                newname = splittext[1]
                 console.log('new name to ',newname)
                 Line.updateOne({lineid:lineid},{nama:newname}).then(() =>{
                     message={
                         type:'text',
-                        text:`Renamed successfully!, welcome ${newname}`
+                        text:`Renamed successfully!, welcome ${newname}`,
                     }
                     console.log('Name changed successfully!')
+                    return message
                 })
             }
         }).then((message)=>{
